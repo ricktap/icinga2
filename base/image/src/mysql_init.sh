6fecdfeb2c88ce6fb404a5d78c1f52ca32e724a3
Patrick Selge <patrick.selge@adesso.de>
2017-07-30 21:20:01 +0200

    user environment vars for mysql

--- base/image/src/mysql_init.sh
+++ base/image/src/mysql_init.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 set -e
 
-ROOT_PASS=password
-USER=icinga
-PASS=icinga
+HOST=${DB_HOST:mysql}
+
+ROOT_PASS=${DB_ROOT_PASSWORD:password}
+USER=${DB_ICINGA_USER:icinga}
+PASS=${DB_ICINGA_PASSWORD:icinga}
 DB=icinga
 
-mysql -h mysql -u root --password=${ROOT_PASS} -e \
+mysql -h ${HOST} -u root --password=${ROOT_PASS} -e \
     "CREATE DATABASE IF NOT EXISTS ${DB};"
-mysql -h mysql -u root --password=${ROOT_PASS} -e \
+mysql -h ${HOST} -u root --password=${ROOT_PASS} -e \
     "GRANT SELECT, INSERT, UPDATE, DELETE, DROP, CREATE VIEW, INDEX, EXECUTE ON ${DB}.* TO '${USER}' IDENTIFIED BY '${PASS}';"
 
 # only run the migration, if the tables to not exist (the icinga mysql scripts are not idempotent)
-if [[ ! $(mysql -h mysql -u root --password=${ROOT_PASS} -e "desc ${DB}.icinga_servicedependencies" > /dev/null 2>&1) -eq "0" ]]; then
-    mysql -h mysql -u root --password=${ROOT_PASS} ${DB} < /share/icinga2-ido-mysql/schema/mysql.sql
+if [[ ! $(mysql -h ${HOST} -u root --password=${ROOT_PASS} -e "desc ${DB}.icinga_servicedependencies" > /dev/null 2>&1) -eq "0" ]]; then
+    mysql -h ${HOST} -u root --password=${ROOT_PASS} ${DB} < /share/icinga2-ido-mysql/schema/mysql.sql
 fi
